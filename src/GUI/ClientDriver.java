996479bed0ba116b2e4f41d84a1396fee2c2851d
Sarim Qureshi <52934393+N19htw1n9@users.noreply.github.com>
2020-10-27 16:29:55 -0500

    Welcome screen GUI implemented, ActionEvents class needs to be implemented and added to the fxml file

---
+++ src/GUI/ClientDriver.java
@@ -0,0 +1,25 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientDriver extends Application
+{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("ClientGUI.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 450, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
