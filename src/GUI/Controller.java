31fedd2840bd69e588d9348b65019594d977e856
Sarim Qureshi <52934393+N19htw1n9@users.noreply.github.com>
2020-10-28 23:48:44 -0500

    WelcomeScreen TextFields MouseClick feature added

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -4,29 +4,51 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 
 public class Controller
 {
-    @FXML
-    private TextField portNumberText;
+    @FXML private TextField portNumberText;
+    @FXML private TextField ipAddressText;
 
-    @FXML
-    private TextField ipAddressText;
 
-    @FXML
-    public void MouseClickActionIP(ActionEvent e)
+    public void MouseClickActionIP()
     {
         ipAddressText.setStyle("-fx-text-fill: black");
         ipAddressText.setFont(Font.font("System", FontWeight.NORMAL, 12));
-        ipAddressText.setText("");
+
+        if((ipAddressText.getText()).equals("IP Address"))
+            ipAddressText.setText("");
     }
 
-    @FXML
-    public void MouseClickActionPN(ActionEvent e)
+    public void MouseClickActionPN()
     {
         portNumberText.setStyle("-fx-text-fill: black");
         portNumberText.setFont(Font.font("System", FontWeight.NORMAL, 12));
-        portNumberText.setText("");
+
+        if((portNumberText.getText()).equals("Port Number"))
+            portNumberText.setText("");
+
+    }
+
+    public void MouseExitActionIP()
+    {
+        if((ipAddressText.getText()).equals(""))
+        {
+            ipAddressText.setText("IP Address");
+            ipAddressText.setStyle("-fx-text-fill: gray");
+            ipAddressText.setFont(Font.font("System Italic", FontPosture.ITALIC, 12));
+        }
+    }
+
+    public void MouseExitActionPN()
+    {
+        if((portNumberText.getText()).equals(""))
+        {
+            portNumberText.setText("Port Number");
+            portNumberText.setStyle("-fx-text-fill: gray");
+            portNumberText.setFont(Font.font("System Italic", FontPosture.ITALIC, 12));
+        }
     }
 }
