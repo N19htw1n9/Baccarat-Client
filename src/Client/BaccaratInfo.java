2dc61f728b7c247eb83501380e06130f48916ee3
ayaanqui <moahammedayaan.dev@gmail.com>
2020-11-01 12:37:52 -0600

    Added BaccaratInfo

---
+++ src/Client/BaccaratInfo.java
@@ -0,0 +1,13 @@
+package Client;
+
+import java.io.Serializable;
+
+public class BaccaratInfo implements Serializable {
+    public int bid;
+    public String hand;
+
+    public BaccaratInfo(int bid, String hand) {
+        this.bid = bid;
+        this.hand = hand;
+    }
+}
