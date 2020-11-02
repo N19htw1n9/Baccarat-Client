3fce3373670eedb3350b47c9c566612576af0989
ayaanqui <moahammedayaan.dev@gmail.com>
2020-11-02 10:21:47 -0600

    Initialized fields, added default serialVersionUID

--- src/BaccaratGame/BaccaratInfo.java
+++ src/BaccaratGame/BaccaratInfo.java
@@ -4,15 +4,17 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class BaccaratInfo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     public double bid;
     public String hand;
 
-    public ArrayList<String> playerHand; // ["1C", "12H"]
-    public ArrayList<String> bankerHand; // ["3D", "4S"]
+    public ArrayList<String> playerHand = new ArrayList<>(); // ["1C", "12H"]
+    public ArrayList<String> bankerHand = new ArrayList<>(); // ["3D", "4S"]
 
-    public String winner;
+    public String winner = "";
 
-    public double winnings;
+    public double winnings = 0.0;
 
     public BaccaratInfo(double bid, String hand) {
         this.bid = bid;
