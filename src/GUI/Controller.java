10177bb51e1b8370d723c8710684856548f85f90
ayaanqui <moahammedayaan.dev@gmail.com>
2020-11-02 14:48:25 -0600

    Added round stats list

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,9 +1,13 @@
 package GUI;
 
+import BaccaratGame.BaccaratInfo;
 import javafx.scene.control.ToggleGroup;
 import BaccaratGame.ConnectionSocket;
+
+import java.util.ArrayList;
 
 public class Controller {
     protected ToggleGroup group;
     protected static ConnectionSocket connection;
+    protected ArrayList<BaccaratInfo> roundStatsList = new ArrayList<>();
 }
