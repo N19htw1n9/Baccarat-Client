f641a7bca1e29e6af2de07b9ec08c6bfcd9f7647
ayaanqui <moahammedayaan.dev@gmail.com>
2020-11-02 13:23:46 -0600

    Set input field prompt

--- src/GUI/WelcomeScreen/WelcomeScreenController.java
+++ src/GUI/WelcomeScreen/WelcomeScreenController.java
@@ -10,9 +10,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import GUI.Controller;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,38 +17,6 @@ import java.io.IOException;
 public class WelcomeScreenController extends Controller {
     @FXML private TextField portNumberText;
     @FXML private TextField ipAddressText;
-
-    public void mouseClickActionIP() {
-        ipAddressText.setStyle("-fx-text-fill: black");
-        ipAddressText.setFont(Font.font("System", FontWeight.NORMAL, 14));
-
-        if ((ipAddressText.getText()).equals("IP Address"))
-            ipAddressText.setText("");
-    }
-
-    public void mouseExitActionIP() {
-        if ((ipAddressText.getText()).equals("")) {
-            ipAddressText.setText("IP Address");
-            ipAddressText.setStyle("-fx-text-fill: gray");
-            ipAddressText.setFont(Font.font("System Italic", FontPosture.ITALIC, 14));
-        }
-    }
-
-    public void mouseClickActionPN() {
-        portNumberText.setStyle("-fx-text-fill: black");
-        portNumberText.setFont(Font.font("System", FontWeight.NORMAL, 14));
-
-        if ((portNumberText.getText()).equals("Port Number"))
-            portNumberText.setText("");
-    }
-
-    public void mouseExitActionPN() {
-        if ((portNumberText.getText()).equals("")) {
-            portNumberText.setText("Port Number");
-            portNumberText.setStyle("-fx-text-fill: gray");
-            portNumberText.setFont(Font.font("System Italic", FontPosture.ITALIC, 14));
-        }
-    }
 
     public void connectButtonAction(Event e) throws IOException {
         System.out.println("Connect button clicked");
